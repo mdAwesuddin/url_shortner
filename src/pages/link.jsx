cfd2033d7a203eefab93054f36cea162ea5b81be
mdAwesuddin <mdawesuddin@gmail.com>
2024-09-07 16:37:35 +0530

    Url changed

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -70,11 +70,11 @@ const Link = () => {
             {url?.title}
           </span>
           <a
-            href={`https://trimrr.in/${link}`}
+            href={`https://url-shortner-psi-taupe.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://trimrr.in/{link}
+            https://url-shortner-psi-taupe.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -92,7 +92,7 @@ const Link = () => {
               variant="ghost"
               onClick={() =>
                 navigator.clipboard.writeText(
-                  `https://trimrr.in/${url?.short_url}`
+                  `https://url-shortner-psi-taupe.vercel.app/${url?.short_url}`
                 )
               }
             >
